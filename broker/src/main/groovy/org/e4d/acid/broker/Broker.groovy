43565ae8ff47df60ce03ae081fb3d57e3dbf0590
eduardi-earnin <eduard.iablonskyi@earnin.com>
2020-03-21 20:56:06 +0200

    capture messages

--- broker/src/main/groovy/org/e4d/acid/broker/Broker.groovy
+++ broker/src/main/groovy/org/e4d/acid/broker/Broker.groovy
@@ -1,8 +1,26 @@
 package org.e4d.acid.broker
 
 import org.zeromq.ZContext
+import org.zeromq.ZFrame
 import org.zeromq.ZMQ
 import org.zeromq.ZMQ.Socket
+import org.zeromq.ZThread
+import org.zeromq.ZThread.IAttachedRunnable
+
+class Listener implements IAttachedRunnable
+{
+  @Override
+  void run(Object[] args, ZContext ctx, Socket pipe)
+  {
+    while (true) {
+      ZFrame frame = ZFrame.recvFrame(pipe);
+      if (frame == null)
+        break;
+      frame.print(null);
+      frame.destroy();
+    }
+  }
+}
 
 class Broker {
   final context = new ZContext()
@@ -17,16 +35,17 @@ class Broker {
 
   Socket getFrontend() {
     final socket = context.createSocket(ZMQ.XSUB)
-    socket.bind('tcp://*:5551')
+    socket.connect('tcp://localhost:6001')
     return socket
   }
 
   Socket getBackend() {
     final socket = context.createSocket(ZMQ.XPUB)
-    socket.bind('tcp://*:5552')
+    socket.bind('tcp://*:6002')
     return socket
   }
 
   Socket getCapture() {
+    ZThread.fork(context, new Listener())
   }
 }
