92e9546d3e8874c627fead1bae5158b2c25ee22c
eduardi-earnin <eduard.iablonskyi@earnin.com>
2020-03-22 21:47:57 +0200

    set monitor to use zmq actor message queue

--- monitor/src/main/groovy/org/e4d/acid/monitor/Monitor.groovy
+++ monitor/src/main/groovy/org/e4d/acid/monitor/Monitor.groovy
@@ -3,12 +3,17 @@ package org.e4d.acid.monitor
 import org.e4d.acid.actor.*
 import org.e4d.acid.domain.actor.*
 import org.e4d.acid.domain.message.*
+import org.e4d.acid.zmq.actor.*
 
 class Monitor {
   static void main(String[] args) {
     final actors = new ActorSystem(
       'monitor',
       StandardOutputActor,
+      queue: new ZmqActorMessageQueue(
+        incoming: 'tcp://localhost:6002',
+        outgoing: 'tcp://localhost:6001',
+      )
     )
     actors.dispatch(
       new OutputTextMessage(text: "acid ${ actors.name } 1.0.0\n"),
