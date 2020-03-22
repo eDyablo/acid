7e602b9225515c37e1bb75534cc1b22ffbf29514
eduardi-earnin <eduard.iablonskyi@earnin.com>
2020-03-22 22:06:35 +0200

    set server to use zmq actor message queue

--- server/src/main/groovy/org/e4d/acid/server/Server.groovy
+++ server/src/main/groovy/org/e4d/acid/server/Server.groovy
@@ -4,18 +4,16 @@ import org.e4d.acid.actor.*
 import org.e4d.acid.domain.actor.*
 import org.e4d.acid.domain.message.*
 import org.e4d.acid.zmq.actor.*
-import org.e4d.acid.zmq.message.*
 
-class Server extends Actor {
-  Server(ActorSystem system) {
-    super(system)
-  }
-
+class Server {
   static void main(String[] args) {
     final actors = new ActorSystem(
       'server',
       StandardOutputActor,
-      Server,
+      queue: new ZmqActorMessageQueue(
+        incoming: 'tcp://localhost:6002',
+        outgoing: 'tcp://localhost:6001',
+      )
     )
     actors.dispatch(
       new OutputTextMessage(text: "acid ${ actors.name } 1.0.0\n"),
