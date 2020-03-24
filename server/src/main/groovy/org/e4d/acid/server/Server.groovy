48bdf516b9a131bd190d7c28c61e2348d9015119
eduardi-earnin <eduard.iablonskyi@earnin.com>
2020-03-24 10:52:25 +0200

    extract methods

--- server/src/main/groovy/org/e4d/acid/server/Server.groovy
+++ server/src/main/groovy/org/e4d/acid/server/Server.groovy
@@ -6,15 +6,27 @@ import org.e4d.acid.domain.message.*
 import org.e4d.acid.zmq.actor.*
 
 class Server {
+  final ActorSystem actors
+  
   static void main(String[] args) {
-    final actors = new ActorSystem(
+    new Server(
+      incoming: 'tcp://localhost:6002',
+      outgoing: 'tcp://localhost:6001',
+    ).run()
+  }
+
+  Server(Map options) {
+    actors = new ActorSystem(
       'server',
       StandardOutputActor,
       queue: new ZmqActorMessageQueue(
-        incoming: 'tcp://localhost:6002',
-        outgoing: 'tcp://localhost:6001',
+        incoming: options.incoming,
+        outgoing: options.outgoing,
       )
     )
+  }
+    
+  void run() {
     actors.dispatch(
       actors.selfMessage(OutputTextMessage, text: "acid ${ actors.name } 1.0.0\n"),
     )
