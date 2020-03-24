b76c0833ef9557760babb641d9f6f92daac81db1
eduardi-earnin <eduard.iablonskyi@earnin.com>
2020-03-24 11:04:40 +0200

    extract console methods

--- console/src/main/groovy/org/e4d/acid/console/Console.groovy
+++ console/src/main/groovy/org/e4d/acid/console/Console.groovy
@@ -8,17 +8,29 @@ import org.e4d.acid.domain.message.*
 import org.e4d.acid.zmq.actor.*
 
 class Console {
+  final ActorSystem actors
+
   static void main(String[] args) {
-    final actors = new ActorSystem(
+    new Console(
+      incoming: 'tcp://localhost:6002',
+      outgoing: 'tcp://localhost:6001',
+    ).run()
+  }
+
+  Console(Map options) {
+    actors = new ActorSystem(
       'console',
       StandardOutputActor,
       CommandActor,
       ConsoleActor,
       queue: new ZmqActorMessageQueue(
-        incoming: 'tcp://localhost:6002',
-        outgoing: 'tcp://localhost:6001',
+        incoming: options.incoming,
+        outgoing: options.outgoing,
       )
     )
+  }
+
+  void run() {
     actors.dispatch(
       actors.selfMessage(OutputTextMessage, text: "acid ${ actors.name } 1.0.0\n"),
       actors.selfMessage(OutputTextMessage, text: 'type \'help\'\n'),
